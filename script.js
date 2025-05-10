442db7da00fe123a5d9def935d5df80105b94aac
Steve Ngai Chee Weng <findingsteve@gmail.com>
2025-05-10 23:30:42 +0800

    .

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const swiper = new Swiper('.swiper', {
     loop: false,
     autoplay: {
-        delay: 3000, // default delay for all slides after the first
+        delay: 5000, // default delay for all slides after the first
         disableOnInteraction: false
     }
 });
@@ -13,4 +13,4 @@ swiper.autoplay.stop();
 setTimeout(() => {
     swiper.slideNext();           // go to second slide
     swiper.autoplay.start();      // resume autoplay
-}, 40000); // 10 seconds
+}, 30000); // 10 seconds
